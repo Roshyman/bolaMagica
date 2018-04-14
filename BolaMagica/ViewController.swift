076d88ad3cf1b6c7fc434934dced0d4965fbb64e
Ronald Murillo Solano <ronaldmurillosolano@estacion-200.ucenfotec.local>
2018-04-14 15:08:32 -0600

    iniciar

--- BolaMagica/ViewController.swift
+++ BolaMagica/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let arregloBolas : [String] = ["ball1","ball2","ball3","ball4","ball5"]
+    
+    @IBOutlet weak var imgBola: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnEjecuta(_ sender: UIButton) {
+        
+        var indice : Int = 0
+        indice = genValor()
+        imgBola.image = UIImage(named: arregloBolas[indice])
+        
+    
+    
+    }
+    
+    func genValor() -> Int{
+        var valor : Int = 10
+    
+        while valor > 4 {
+            valor = Int(arc4random_uniform(15))
+        }
+        return valor
+    }
 
 }
 
